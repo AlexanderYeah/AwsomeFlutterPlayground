9c812e9c49313826ddbefebec1625d415c4e9c11
Alexander <1023954998@qq.com>
2023-03-17 14:23:09 +0800

    补间动画补全

--- awsome_flutter/lib/jiezhi/animation/an_animation.dart
+++ awsome_flutter/lib/jiezhi/animation/an_animation.dart
@@ -1,3 +1,4 @@
+import 'package:awsome_flutter/service/screen_adapter.dart';
 import 'package:flutter/material.dart';
 
 import '../../util/color.dart';
@@ -14,19 +15,27 @@ class _AnimationDemoPageState extends State<AnimationDemoPage>
     with TickerProviderStateMixin {
   // 配置控制器
   AnimationController? _animationController;
+  // 大小
   Animation<double>? _animation;
+  // 颜色
   Animation? _colorAnimation;
-
+  // 位移
+  Animation<Offset>? _offsetAnimation;
+  // 圆角动画
+  Animation<BorderRadius>? _radiusAnimation;
+  // 字体样式动画
+  Animation<TextStyle>? _textStyleAnimation;
+  // decoration 动画
+  Animation<Decoration>? _decorationAnimation;
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
-        duration: Duration(milliseconds: 1000), vsync: this);
+        duration: Duration(milliseconds: 2000), vsync: this);
     // 放大缩小的动画
     _animation =
         Tween<double>(begin: 25, end: 50).animate(_animationController!)
           ..addListener(() {
-            print(_animation!.value);
             setState(() {});
           });
     // 颜色动画
@@ -39,13 +48,41 @@ class _AnimationDemoPageState extends State<AnimationDemoPage>
           })
           ..addStatusListener((status) {
             if (status == AnimationStatus.completed) {
-              print("动画完成");
               _animationController!.reverse();
             } else if (status == AnimationStatus.dismissed) {
               _animationController!.forward();
             }
           });
-
+    // 位移动画
+    _offsetAnimation = Tween<Offset>(begin: Offset(0, 0), end: Offset(100, 100))
+        .animate(_animationController!)
+      ..addListener(() {
+        setState(() {});
+      });
+    // 圆角动画
+    _radiusAnimation = Tween<BorderRadius>(
+            begin: BorderRadius.circular(0), end: BorderRadius.circular(50))
+        .animate(_animationController!)
+      ..addListener(() {
+        setState(() {});
+      });
+    //字体样式动画
+    _textStyleAnimation = TextStyleTween(
+            begin: TextStyle(color: Colors.orange, fontSize: 15),
+            end: TextStyle(color: Colors.redAccent, fontSize: 30))
+        .animate(_animationController!)
+      ..addListener(() {
+        setState(() {});
+      });
+    _decorationAnimation = DecorationTween(
+            begin: BoxDecoration(
+                color: Colors.black45, borderRadius: BorderRadius.circular(5)),
+            end: BoxDecoration(
+                color: Colors.cyan, borderRadius: BorderRadius.circular(50)))
+        .animate(_animationController!)
+      ..addListener(() {
+        setState(() {});
+      });
     // 监听动画完成的时间
     _animationController!.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
@@ -76,19 +113,18 @@ class _AnimationDemoPageState extends State<AnimationDemoPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Animation"),
-        ),
-        body: Container(
-            child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: ElevatedButton(onPressed: () {}, child: Text("开始")),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: IconButton(
+      appBar: AppBar(
+        title: Text("Animation"),
+      ),
+      body: Container(
+          width: ScreenAdapter.width(750),
+          height: ScreenAdapter.height(1334),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              IconButton(
                 // 动画的值 作为size
                 iconSize: _animation!.value,
                 icon: Icon(
@@ -97,16 +133,54 @@ class _AnimationDemoPageState extends State<AnimationDemoPage>
                 ),
                 onPressed: null,
               ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: 100,
-                height: 100,
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 80,
+                height: 80,
                 color: _colorAnimation!.value,
               ),
-            )
-          ],
-        )));
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                color: Colors.cyan,
+                margin: EdgeInsets.only(
+                    left: _offsetAnimation!.value.dx,
+                    top: _offsetAnimation!.value.dy),
+                width: 80,
+                height: 80,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  borderRadius: _radiusAnimation!.value,
+                  color: Colors.purple,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 80,
+                child: Text("Stay Hungry,Stay Foolish",
+                    style: _textStyleAnimation!.value),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: _decorationAnimation!.value,
+              ),
+            ],
+          )),
+    );
   }
 }
